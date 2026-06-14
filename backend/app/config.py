89051945489a8ab3aa38eda8f720430c7e9696fb
Subhash <jettysubhash02@gmail.com>
2026-06-14 17:36:33 +0530

    Auto-detect BACKEND_URL on Render

--- backend/app/config.py
+++ backend/app/config.py
@@ -23,6 +23,14 @@ class Settings(BaseSettings):
     # Frontend
     FRONTEND_URL: str = "http://127.0.0.1:3000"
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        import os
+        # Automatically use Render's external URL if BACKEND_URL is default
+        render_url = os.environ.get("RENDER_EXTERNAL_URL")
+        if render_url and self.BACKEND_URL == "http://127.0.0.1:8000":
+            self.BACKEND_URL = render_url
+
     # App
     DEBUG: bool = False
 
